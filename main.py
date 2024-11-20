b61a38e8a718b5911ccf5d131bdeed384b75a94b
CarlosMonforteIzquierdo <carlosmonforteizquierdo@gmail.com>
2024-11-20 23:06:23 +0100

    @socketio.on y css hecho

--- main.py
+++ main.py
@@ -12,22 +12,26 @@ rooms = {}
 def generate_unique_code(length):
     while True:
         code=""
-        for i in range(length):
+        for _ in range(length):
             code += random.choice(ascii_uppercase)
         if code not in rooms:
             break
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
+    session.clear()
     if request.method== 'POST':
         name = request.form.get("name")
         code = request.form.get("code")
         join = request.form.get("join", False)
         create = request.form.get("create", False)
+
         if not name:
             return render_template("home.html", error="Please enter a name", code=code, name=name)
+        
         if join != False and not code:
             return render_template("home.html", error="Please enter a room code", code=code, name=name)
+        
         room = code
         if create != False:
             room = generate_unique_code(4)
@@ -37,12 +41,32 @@ def home():
 
         session['room'] = room
         session['name'] = name
-
         return redirect(url_for("room"))
+    
     return render_template("home.html")
 
-app.route("/room")
+@app.route("/room")
 def room():
-    return render_template('room.html')
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))
+
+    return render_template("room.html")
+
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room ")
+    name = session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    
+    join_room(room)
+    send({"name": name, "message": "has entered the room"}, to=room)
+    rooms[room]["members"] += 1
+    print(f"{name} joinder room {room}")
+
 if __name__ == '__main__':
     socketio.run(app, debug=True,allow_unsafe_werkzeug=True)
